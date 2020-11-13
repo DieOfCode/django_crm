6a9120f56ba9f71e909b1b72fb94beb321af3898
DieOfCode <fr0ststone878@gmail.com>
2020-11-13 11:00:36 +0500

    add to deploy

--- shopPage/settings.py
+++ shopPage/settings.py
@@ -13,8 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -22,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '98*vhs6i@8)m9asz&9t3gx@_ergp6s76j4dbfo$%0a95r%(o*5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -41,6 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,15 +72,22 @@ WSGI_APPLICATION = 'shopPage.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "DEMO_TEST",
-        "USER": "postgres",
-        "PASSWORD": "vanish878",
-        "HOST": "localhost",
-        "PORT": "5432"
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': "DEMO_TEST",
+#         "USER": "postgres",
+#         "PASSWORD": "vanish878",
+#         "HOST": "localhost",
+#         "PORT": "5432"
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -119,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = "/images/"
 
@@ -132,5 +138,10 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_TIMEOUT = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = "fr0stsone878@gmail.com"
-EMAIL_HOST_USER = "FordmustangshelbyGT5501971"
+EMAIL_HOST_PASSWORD = "************"
+EMAIL_HOST_USER = "*************"
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['ivansvirskiycrm1.herokuapp.com', '127.0.0.1 ']
